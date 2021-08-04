0f7ad92c44e96004b221cb3ed2bf07d2c5836515
RaphaelSmith <thetutorialdoctor@gmail.com>
2021-08-04 13:43:19 -0400

    work page updaet

---
+++ javascripts/data.js
@@ -0,0 +1,102 @@
+const data = [
+    {
+        name: "Portfolio",
+        image: "http://tutorialdoctor.github.io/images/portfolio.png",
+        description: "A portfolio page built with HTML, CSS and JS in 2020"
+    },
+    {
+        name: "Youtube Downloader",
+        image: "http://tutorialdoctor.github.io/images/downloader.png",
+        description: "Youtube downloader built with python flask and styled with Tailwind CSS in 2021"
+    },
+    {
+        name: "Nextflix1",
+        image: "http://tutorialdoctor.github.io/images/net1.png",
+        description: "Netflix clone buildt with VueJS with ruby,python and node servers and styled with Tailwind CSS"
+    },
+    {
+        name: "Nextflix2",
+        image: "http://tutorialdoctor.github.io/images/net2.png",
+        description: "Netflix clone buildt with VueJS with ruby,python and node servers and styled with Tailwind CSS"
+    },
+    {
+        name: "Nextflix3",
+        image: "http://tutorialdoctor.github.io/images/net3.png",
+        description: "Netflix clone buildt with VueJS with ruby,python and node servers and styled with Tailwind CSS"
+    },
+    {
+        name: "RealEstate",
+        image: "http://tutorialdoctor.github.io/images/realestate.png",
+        description: "Prototype with Tailwind CSS"
+    },
+    {
+        name: "DreamCenter",
+        image: "http://tutorialdoctor.github.io/images/dream.png",
+        description: "Client website built with VueJS and styled with Bootstrap"
+    },
+    {
+        name: "Mandalorian",
+        image: "http://tutorialdoctor.github.io/images/manda.png",
+        description: "Prototype with Tailwind"
+    },
+    {
+        name: "VueStore",
+        image: "http://tutorialdoctor.github.io/images/ecommerce.JPG",
+        description: "Ecommerce app built with VueJS and styled with Tailwind"
+    },
+    {
+        name: "LMS1",
+        image: "http://tutorialdoctor.github.io/images/lms1.png",
+        description: "Learning management system build with VueJS and styled with Tailwind"
+    },
+    {
+        name: "LMS2",
+        image: "http://tutorialdoctor.github.io/images/lms2.png",
+        description: "Learning management system build with VueJS and styled with Tailwind"
+    },
+    {
+        name: "GameUI",
+        image: "http://tutorialdoctor.github.io/images/gameui.png",
+        description: "Prototype with HTML, CSS and JS"
+    },
+    {
+        name: "Pukka1",
+        image: "http://tutorialdoctor.github.io/images/pukka1.png",
+        description: "Ruby on Rails app with Tailwind CSS and other technologies"
+    },
+    {
+        name: "Pukka2",
+        image: "http://tutorialdoctor.github.io/images/pukka2.png",
+        description: "Ruby on Rails app with Tailwind CSS and other technologies"
+    },
+    {
+        name: "Pukka3",
+        image: "http://tutorialdoctor.github.io/images/schedule.png",
+        description: "Ruby on Rails app with Tailwind CSS and other technologies"
+    },
+    {
+        name: "Photography",
+        image: "http://tutorialdoctor.github.io/images/photography.png",
+        description: "HTML, CSS, JS from template"
+    },
+    {
+        name: "Medemy",
+        image: "http://tutorialdoctor.github.io/images/medemy.png",
+        description: "Udemy clone ptototype with Tailwind CS"
+    },
+    {
+        name: "Docs1",
+        image: "http://tutorialdoctor.github.io/images/docsclone1.png",
+        description: "Functional Google Docs clone with ReactJS, Firebase and Google Auth styled with Tailwind CSS"
+    },
+    {
+        name: "Docs2",
+        image: "http://tutorialdoctor.github.io/images/docsclone2.png",
+        description: "Functional Google Docs clone with ReactJS, Firebase and Google Auth styled with Tailwind CSS"
+    },
+    {
+        name: "9Oak",
+        image: "http://tutorialdoctor.github.io/images/oak.png",
+        description: "Client website with VueJS styled with Bootstrap and including a ZOHO integration"
+    }
+]
