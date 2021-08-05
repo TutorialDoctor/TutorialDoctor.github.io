23e20ee06b4f7d91dfd231e547cd16abc155b79a
RaphaelSmith <thetutorialdoctor@gmail.com>
2021-08-05 14:13:12 -0400

    data update

--- javascripts/data.js
+++ javascripts/data.js
@@ -2,106 +2,127 @@ const data = [
     {
         name: "Portfolio",
         image: "http://tutorialdoctor.github.io/images/portfolio.png",
-        description: "A portfolio page built with HTML, CSS and JS in 2020"
+        description: "A portfolio page built with HTML, CSS and JS in 2020",
+        url:""
     },
     {
         name: "Youtube Downloader",
         image: "http://tutorialdoctor.github.io/images/downloader.png",
-        description: "Youtube downloader built with python flask and styled with Tailwind CSS in 2021"
+        description: "Youtube downloader built with python flask and styled with Tailwind CSS in 2021",
+        url:""
     },
     {
         name: "Nextflix1",
         image: "http://tutorialdoctor.github.io/images/net1.png",
-        description: "Netflix clone buildt with VueJS with ruby,python and node servers and styled with Tailwind CSS"
+        description: "Netflix clone buildt with VueJS with ruby,python and node servers and styled with Tailwind CSS",
+        url:""
     },
     {
         name: "Nextflix2",
         image: "http://tutorialdoctor.github.io/images/net2.png",
-        description: "Netflix clone buildt with VueJS with ruby,python and node servers and styled with Tailwind CSS"
+        description: "Netflix clone buildt with VueJS with ruby,python and node servers and styled with Tailwind CSS",
+        url:""
     },
     {
         name: "Nextflix3",
         image: "http://tutorialdoctor.github.io/images/net3.png",
-        description: "Netflix clone buildt with VueJS with ruby,python and node servers and styled with Tailwind CSS"
+        description: "Netflix clone buildt with VueJS with ruby,python and node servers and styled with Tailwind CSS",
+        url:""
     },
     {
         name: "RealEstate",
         image: "http://tutorialdoctor.github.io/images/realestate.png",
-        description: "Prototype with Tailwind CSS"
+        description: "Prototype with Tailwind CSS",
+        url:""
     },
     {
         name: "DreamCenter",
         image: "http://tutorialdoctor.github.io/images/dream.png",
-        description: "Client website built with VueJS and styled with Bootstrap"
+        description: "Client website built with VueJS and styled with Bootstrap",
+        url:""
     },
     {
         name: "Mandalorian",
         image: "http://tutorialdoctor.github.io/images/manda.png",
-        description: "Prototype with Tailwind"
+        description: "Prototype with Tailwind",
+        url:""
     },
     {
         name: "VueStore",
         image: "http://tutorialdoctor.github.io/images/ecommerce.JPG",
-        description: "Ecommerce app built with VueJS and styled with Tailwind"
+        description: "Ecommerce app built with VueJS and styled with Tailwind",
+        url:""
     },
     {
         name: "LMS1",
         image: "http://tutorialdoctor.github.io/images/lms1.png",
-        description: "Learning management system build with VueJS and styled with Tailwind"
+        description: "Learning management system build with VueJS and styled with Tailwind",
+        url:""
     },
     {
         name: "LMS2",
         image: "http://tutorialdoctor.github.io/images/lms2.png",
-        description: "Learning management system build with VueJS and styled with Tailwind"
+        description: "Learning management system build with VueJS and styled with Tailwind",
+        url:""
     },
     {
         name: "GameUI",
         image: "http://tutorialdoctor.github.io/images/gameui.png",
-        description: "Prototype with HTML, CSS and JS"
+        description: "Prototype with HTML, CSS and JS",
+        url:""
     },
     {
         name: "Pukka1",
         image: "http://tutorialdoctor.github.io/images/pukka1.png",
-        description: "Ruby on Rails app with Tailwind CSS and other technologies"
+        description: "Ruby on Rails app with Tailwind CSS and other technologies",
+        url:""
     },
     {
         name: "Pukka2",
         image: "http://tutorialdoctor.github.io/images/pukka2.png",
-        description: "Ruby on Rails app with Tailwind CSS and other technologies"
+        description: "Ruby on Rails app with Tailwind CSS and other technologies",
+        url:""
     },
     {
         name: "Pukka3",
         image: "http://tutorialdoctor.github.io/images/schedule.png",
-        description: "Ruby on Rails app with Tailwind CSS and other technologies"
+        description: "Ruby on Rails app with Tailwind CSS and other technologies",
+        url:""
     },
     {
         name: "Photography",
         image: "http://tutorialdoctor.github.io/images/photography.png",
-        description: "HTML, CSS, JS from template"
+        description: "HTML, CSS, JS from template",
+        url:""
     },
     {
         name: "Medemy",
         image: "http://tutorialdoctor.github.io/images/medemy.png",
-        description: "Udemy clone ptototype with Tailwind CS"
+        description: "Udemy clone ptototype with Tailwind CS",
+        url:""
     },
     {
         name: "Docs1",
         image: "http://tutorialdoctor.github.io/images/docsclone1.png",
-        description: "Functional Google Docs clone with ReactJS, Firebase and Google Auth styled with Tailwind CSS"
+        description: "Functional Google Docs clone with ReactJS, Firebase and Google Auth styled with Tailwind CSS",
+        url: "https://google-docs-clone-lake-six.vercel.app/"
     },
     {
         name: "Docs2",
         image: "http://tutorialdoctor.github.io/images/docsclone2.png",
-        description: "Functional Google Docs clone with ReactJS, Firebase and Google Auth styled with Tailwind CSS"
+        description: "Functional Google Docs clone with ReactJS, Firebase and Google Auth styled with Tailwind CSS",
+        url: "https://google-docs-clone-lake-six.vercel.app/"
     },
     {
         name: "9Oak",
         image: "http://tutorialdoctor.github.io/images/oak.png",
-        description: "Client website with VueJS styled with Bootstrap and including a ZOHO integration"
+        description: "Client website with VueJS styled with Bootstrap and including a ZOHO integration",
+        url:""
     },
     {
         name: "Portal",
-        image: "../images/portal.png",
-        description: "Client website with VueJS styled with Bootstrap and including a ZOHO integration"
+        image: "https://tutorialdoctor.github.io/images/portal.png",
+        description: "Client website with VueJS styled with Bootstrap and including a ZOHO integration",
+        url:""
     }
 ]
